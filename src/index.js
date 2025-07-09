330b4293be28778ff8ae3f8f6f295faf4869616f
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-07-09 19:02:12 +0530

    Initialize backend project with essential files and configurations

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './db/index.js';
+
+dotenv.config();
+const app = express();
+
+connectDB()
+.then(() => {
+    app.listen(3000, () => {
+       console.log('====================================');
+       console.log("Server is running on port 3000");
+       console.log('====================================');
+    })
+})
+.catch((error) => {
+    console.error('Error connecting to the database:', error);
+    process.exit(1); // Exit process with failure
+});
